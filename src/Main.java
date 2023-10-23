94b94b8eb0ead42fb3169dc2a2eab78a88520148
manoj1pradhan <pradhamj@mail.uc.edu>
2023-10-22 23:10:53 -0400

    v1.0

---
+++ src/Main.java
@@ -0,0 +1,27 @@
+class CountingLoops {
+    public static void main(String[] args) {
+        System.out.println("Task 1:");
+        for (int i = 0; i <= 30; i++) {
+            System.out.print(i + " ");
+        }
+        System.out.println("\n");
+
+        System.out.println("Task 2:");
+        for (int i = 30; i >= 0; i--) {
+            System.out.print(i + " ");
+        }
+        System.out.println("\n");
+
+        System.out.println("Task 3:");
+        for (int i = 0; i <= 18; i += 3) {
+            System.out.print(i + " ");
+        }
+        System.out.println("\n");
+
+        System.out.println("Task 4:");
+        for (int i = 10; i >= 0; i -= 2) {
+            System.out.print(i + " ");
+        }
+        System.out.println("\n");
+    }
+}
